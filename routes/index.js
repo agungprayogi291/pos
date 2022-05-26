1f890d271db03b4fb312d118e6ea40dac6dca205
agungprayogi291 <agungprayogi291@gmail.com>
2022-05-26 20:19:09 +0700

    [add] Route json welcome

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ title: 'POS RestAPi', message:'Welcome' });
 });
+
 
 module.exports = router;
