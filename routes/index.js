2c9ccead1aa1ce111f437a666bc41ec514968067
agungprayogi291 <agungprayogi291@gmail.com>
2022-05-27 20:21:58 +0700

    [Add] get brand

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const app = express()
+const user = require('./../controllers/user');
+const brand = require('./../controllers/brand');
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ title: 'POS RestAPi', message:'Welcome' });
+  res.json({ title: 'POS RestAPi', message:'Welcome ' });
 });
-
-
+// router.get('/user',user)
+router.get('/brand',brand.getAll);
 module.exports = router;
